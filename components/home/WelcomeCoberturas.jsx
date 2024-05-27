b762459a5d793b65d8c41ad587c0f150bb3ad31d
francisco <franvitar15@gmail.com>
2024-05-27 18:37:48 -0300

    fix

--- components/home/WelcomeCoberturas.jsx
+++ components/home/WelcomeCoberturas.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import "../styles/_welcome.scss";
 import { motion } from "framer-motion";
 
@@ -14,20 +15,37 @@ const textVariants = {
 };
 
 function WelcomeCoberturas({ setSearchTerm, searchTerm }) {
+  const [coberturas, setCoberturas] = useState("");
+
+  const getCoberturas = (valor) => {
+    setCoberturas(valor);
+  };
+
+  const handleSearchClick = () => {
+    setSearchTerm(coberturas);
+    window.scrollBy({
+      top: 500, // ajusta esta cantidad según sea necesario
+      behavior: "smooth",
+    });
+  };
   return (
-    <div id="inicio" className="home-welcome">
+    <div id="inicio" className="home-welcome coberturas-welcome">
       <div className="contenedor">
         <motion.h2 {...textVariants}>OBRAS SOCIALES</motion.h2>
         <motion.p {...textVariants}>
           Atendemos más de 50 obras sociales
         </motion.p>
+
         <motion.input
           {...textVariants}
           type="text"
           placeholder="Buscar..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          value={coberturas}
+          onChange={(e) => getCoberturas(e.target.value)}
         />
+        <motion.a {...textVariants} onClick={handleSearchClick}>
+          Buscar
+        </motion.a>
       </div>
     </div>
   );
--- components/home/WelcomePreguntas.jsx
+++ components/home/WelcomePreguntas.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import "../styles/_welcome.scss";
 import { motion } from "framer-motion";
 
@@ -13,17 +14,34 @@ const textVariants = {
   },
 };
 function WelcomePreguntas({ setSearchTerm, searchTerm }) {
+  const [preguntas, setPreguntas] = useState("");
+
+  const getPreguntas = (valor) => {
+    setPreguntas(valor);
+  };
+
+  const handleSearchClick = () => {
+    setSearchTerm(preguntas);
+    window.scrollBy({
+      top: 200, // ajusta esta cantidad según sea necesario
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div id="inicio" className="home-welcome">
+    <div id="inicio" className="home-welcome preguntas-welcome">
       <div className="contenedor">
         <motion.h2 {...textVariants}>PREGUNTAS FRECUENTES</motion.h2>
         <motion.input
           {...textVariants}
           type="text"
           placeholder="Buscar..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          value={preguntas}
+          onChange={(e) => getPreguntas(e.target.value)}
         />
+        <motion.a {...textVariants} onClick={handleSearchClick}>
+          Buscar
+        </motion.a>
       </div>
     </div>
   );
