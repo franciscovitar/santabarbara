64b9ec0446bd428797f2f64186216a529bb8d5b6
Franciscovitar <franvitar15@gmail.com>
2024-08-14 10:07:57 -0300

    fix

--- app/layout.js
+++ app/layout.js
@@ -33,7 +33,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="DRNtakV0LDpfaauuRo97JC6yl3ZoFvVpY_B5icphBLs"
+        />
+      </head>
+      <body translate="no" className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
